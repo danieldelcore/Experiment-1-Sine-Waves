9ac8a50f68c9a5a548640edb0d6d438f9357ad2e
Daniel Del Core <daniel.delcore.92@gmail.com>
2016-05-08 15:56:58 +1000

    Small logic optimisation

--- src/scripts/plane.js
+++ src/scripts/plane.js
@@ -15,15 +15,14 @@ export default class Plane {
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.rotation.x = -0.5 * Math.PI;
         this.mesh.position.set(0, 0, 0);
+        this.vStart = new THREE.Vector2(0, 0);
+        this.vCount = this.mesh.geometry.vertices.length;
     }
 
     update(timeStamp) {
-        const center = new THREE.Vector2(0, 0);
-        const vCount = this.mesh.geometry.vertices.length;
-
-        for (let i = 0; i < vCount; i++) {
+        for (let i = 0; i < this.vCount; i++) {
             const v = this.mesh.geometry.vertices[i];
-            const dist = new THREE.Vector2(v.x, v.y).sub(center);
+            const dist = new THREE.Vector2(v.x, v.y).sub(this.vStart);
             const size = 50.0;
             const magnitude = 40;
             v.z = Math.sin(dist.length() / -size + (timeStamp / 500)) * magnitude;
