6799a121c0f01123e7328005e5ca0fd0cfd02212
Daniel Del Core <daniel.delcore.92@gmail.com>
2016-05-20 15:47:07 +1000

    Updated three js + added dat gui + refactored main js file + cleanup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,84 +1,124 @@
 /* globals Detector */
+/* eslint-disable no-param-reassign */
 
 import THREE from 'three';
+import dat from 'dat-gui';
 import Plane from './plane';
+import bindAll from 'lodash/bindAll';
+
+const gui = new dat.GUI();
 
 if (!Detector.webgl) {
     Detector.addGetWebGLMessage();
     document.getElementById('container').innerHTML = '';
 }
 
-let container;
-let controls;
-let camera;
-let scene;
-let renderer;
-
-const plane = new Plane();
-
-function onResize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+function initGui(main) {
+    // Lights
+    const folder = gui.addFolder('Light');
+    folder.addColor(main.conf, 'hemisphereLightColor')
+        .onChange(c => main.hemisphereLight.color = new THREE.Color(c));
+    folder.addColor(main.conf, 'hemisphereLightColor2')
+        .onChange(c => main.hemisphereLight.groundColor = new THREE.Color(c));
+    folder.addColor(main.conf, 'hemisphereLightIntensity')
+        .onChange(c => main.hemisphereLight.intensity = new THREE.Color(c));
+    folder.addColor(main.conf, 'directionalLightColor')
+        .onChange(c => main.directionalLight.color = new THREE.Color(c));
+    folder.add(main.conf, 'directionalLightIntensity')
+        .onChange(c => main.directionalLight.intensity = c);
 }
 
-function initRenderer() {
-    renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.setClearColor(0x17293a);
+class Main {
+    constructor() {
+        bindAll(this,
+            'animate',
+            'onResize');
+
+        this.conf = {
+            planeWidth: 4000,
+            planeHeight: 3000,
+            planeWidthSeg: 300,
+            planeHeightSeg: 300,
+            planeShininess: 30,
+            hemisphereLightColor: 0x5F15FF,
+            hemisphereLightColor2: 0x42F58D,
+            hemisphereLightIntensity: 1,
+            directionalLightColor: 0x2f00c8,
+            directionalLightIntensity: 1,
+        };
+
+        // Renderer
+        this.renderer = new THREE.WebGLRenderer({ alpha: true });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        // this.renderer.setClearColor(0x17293a);
+
+        // Container
+        this.container = document.createElement('div');
+        document.body.appendChild(this.container);
+        this.container.appendChild(this.renderer.domElement);
+
+        // Scene
+        this.scene = new THREE.Scene();
+        window.addEventListener('resize', this.onResize);
+
+        // Camera
+        this.camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth / window.innerHeight,
+            0.01,
+            10000);
+        this.camera.position.set(0, 1500, 1500);
+        this.camera.lookAt(0, 0, 0);
+
+        // Controls
+        this.controls = new THREE.OrbitControls(
+            this.camera,
+            this.renderer.domElement);
+        this.controls.userPan = false;
+        this.controls.userPanSpeed = 0.0;
+        this.controls.maxDistance = 5000.0;
+        this.controls.target.set(0, 0, 0);
+
+        // Light
+        this.directionalLight = new THREE.DirectionalLight(
+            this.conf.directionalLightColor,
+            this.conf.directionalLightIntensity);
+        this.directionalLight.position.set(-20, 20, 30);
+        this.hemisphereLight = new THREE.HemisphereLight(
+            this.conf.hemisphereLightColor,
+            this.conf.hemisphereLightColor2,
+            this.conf.hemisphereLightIntensity);
+        this.hemisphereLight.position.set(-20, 20, 30);
+
+        // Pièce de résistance
+        this.plane = new Plane(gui);
+
+        this.scene.add(
+            this.hemisphereLight,
+            this.directionalLight,
+            this.plane.mesh);
+    }
+
+    onResize() {
+        const { innerWidth, innerHeight } = window;
+        this.renderer.setSize(innerWidth, innerHeight);
+        this.camera.aspect = innerWidth / innerHeight;
+        this.camera.updateProjectionMatrix();
+    }
+
+    render(timeStamp) {
+        this.plane.update(timeStamp);
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    animate(timeStamp) {
+        requestAnimationFrame(this.animate);
+        this.render(timeStamp);
+    }
 }
 
-function initCamera() {
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 10000);
-    camera.position.set(0, 1500, 1500);
-    camera.lookAt(scene.position);
-}
-
-function initControls() {
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.userPan = false;
-    controls.userPanSpeed = 0.0;
-    controls.maxDistance = 5000.0;
-    controls.maxPolarAngle = Math.PI * 0.495;
-    controls.center.set(0, 0, 0);
-}
-
-function initLight() {
-    const hemisphereLight = new THREE.HemisphereLight(0x5F15FF, 0x42F58D, 1);
-    hemisphereLight.position.set(-20, 20, 30);
-    scene.add(hemisphereLight);
-}
-
-function init() {
-    initRenderer();
-
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    container.appendChild(renderer.domElement);
-
-    scene = new THREE.Scene();
-    window.addEventListener('resize', onResize);
-
-    initCamera();
-    initControls();
-    initLight();
-
-    scene.add(plane.mesh);
-}
-
-function render(timeStamp) {
-    plane.update(timeStamp);
-    controls.update();
-    renderer.render(scene, camera);
-}
-
-function animate(timeStamp) {
-    requestAnimationFrame(animate);
-    render(timeStamp);
-}
-
-init();
-animate();
+const main = new Main();
+main.animate();
+initGui(main);
--- src/scripts/plane.js
+++ src/scripts/plane.js
@@ -1,30 +1,43 @@
 const THREE = require('three');
 
 export default class Plane {
-    constructor() {
+    constructor(gui) {
         const geometry = new THREE.PlaneGeometry(4000, 800, 300, 300);
         const material = new THREE.MeshPhongMaterial({
-            color: 0xffffff,
-            specular: 0xffffff,
+            color: 0xFFFFFF,
+            specular: 0xFFFFFF,
+            emissive: 0x000000,
             shininess: 30,
             shading: THREE.FlatShading,
             side: THREE.DoubleSide,
             // wireframe: true,
         });
 
+        this.config = {
+            size: 50.0,
+            magnitude: 40,
+        };
+
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.rotation.x = -0.5 * Math.PI;
         this.mesh.position.set(0, 0, 0);
         this.vStart = new THREE.Vector2(0, 0);
         this.vCount = this.mesh.geometry.vertices.length;
+
+        this.initGui(gui);
+    }
+
+    initGui(gui) {
+        const folder = gui.addFolder('Plane');
+        folder.add(this.config, 'size', 10, 250);
+        folder.add(this.config, 'magnitude', 10, 200);
     }
 
     update(timeStamp) {
         for (let i = 0; i < this.vCount; i++) {
+            const { size, magnitude } = this.config;
             const v = this.mesh.geometry.vertices[i];
             const dist = new THREE.Vector2(v.x, v.y).sub(this.vStart);
-            const size = 50.0;
-            const magnitude = 40;
             v.z = Math.sin(dist.length() / -size + (timeStamp / 500)) * magnitude;
         }
 
